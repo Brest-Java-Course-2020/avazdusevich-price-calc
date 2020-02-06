5f0fd9e23142031d8348626441a20bf2b96a77db
Aliaksei Vazdusevich <avozdusevich@gmail.com>
2020-02-06 23:18:22 +0300

    Implemented BigDecimal class

---
+++ avazdusevich-price-calc/src/main/java/com/epam/brest/BigDecimalPriceCalc.java
@@ -0,0 +1,98 @@
+package com.epam.brest;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.Scanner;
+
+public class BigDecimalPriceCalc implements Calculator{
+
+    private BigDecimal distance;
+    private BigDecimal weight;
+    private PriceList priceList = new PriceList();
+
+    private final static String EXIT_VALUE = "Q";
+
+    private final static String ENTER_DISTANCE_MESSAGE = "Please, enter distance or Q for exit: ";
+    private final static String ENTER_WEIGHT_MESSAGE = "Please, enter weight or Q for exit: ";
+    private final static String EXCEPTION_MESSAGE = "Something went wrong!";
+
+    private final static int DISTANCE = 0;
+    private final static int WEIGHT = 1;
+    private final static int FINISH = 2;
+
+    private int iterator = 0;
+
+    @Override
+    public void makeCalculation() {
+        Scanner scanner = new Scanner(System.in);
+        String enteredValue = "";
+
+        while (isNotExitValue(enteredValue)) {
+            dataEntryRequest(iterator);
+            enteredValue = scanner.next();
+            fillUpEnteredValues(enteredValue);
+            finishCalculation();
+        }
+
+        System.out.println("_____________________________________\nAll calculations done.");
+    }
+
+    private boolean isNotExitValue(String value) {
+        return !value.equalsIgnoreCase(EXIT_VALUE);
+    }
+
+    private boolean isCorrectValue(String value) {
+        try {
+            BigDecimal enteredValue = new BigDecimal(value);
+            return enteredValue.compareTo(BigDecimal.valueOf(0)) > 0;
+        } catch (NumberFormatException e) {
+            System.out.println("Invalid value entered.");
+            return false;
+        }
+    }
+
+    private void dataEntryRequest(int requestNumber) {
+        switch (requestNumber) {
+            case (DISTANCE) :
+                System.out.println(ENTER_DISTANCE_MESSAGE);
+                break;
+            case (WEIGHT) :
+                System.out.println(ENTER_WEIGHT_MESSAGE);
+                break;
+            default:
+                System.out.println(EXCEPTION_MESSAGE);
+        }
+    }
+
+    private void fillUpEnteredValues(String value) {
+        if (isNotExitValue(value)) {
+            if (isCorrectValue(value)){
+                if (iterator==DISTANCE) {
+                    distance = new BigDecimal(value);
+                }
+                if (iterator==WEIGHT) {
+                    weight = new BigDecimal(value);
+                }
+                iterator++;
+            } else {
+                System.out.println("Invalid value entered.");
+            }
+        }
+    }
+
+    private void finishCalculation() {
+        if (iterator==FINISH) {
+            BigDecimal result = getResult().setScale(2, RoundingMode.HALF_UP);
+            System.out.println("\nPrise: $" + result);
+            System.out.println("_____________________________________\n");
+            iterator=0;
+        }
+    }
+
+    private BigDecimal getResult() {
+        BigDecimal distancePrice = distance.multiply(priceList.getDistancePrice(distance));
+        BigDecimal weightPrice = weight.multiply(priceList.getLoadPrice(weight));
+        return distancePrice.add(weightPrice);
+    }
+
+}
--- avazdusevich-price-calc/src/main/java/com/epam/brest/PriceList.java
+++ avazdusevich-price-calc/src/main/java/com/epam/brest/PriceList.java
@@ -1,61 +1,59 @@
 package com.epam.brest;
-
 import com.epam.brest.util.MyFileReader;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.ArrayList;
 
 public class PriceList {
 
     static {
-        MyFileReader reader = new MyFileReader("src/main/java/com/epam/brest/sources/PriceList");
+        MyFileReader reader = new MyFileReader("src/main/java/com/epam/brest/resources/PriceList");
         ArrayList<String> priceList = reader.readFromFile();
-        shortDistancePrice = Double.parseDouble(priceList.get(1));
-        mediumDistancePrice = Double.parseDouble(priceList.get(2));
-        longDistancePrice = Double.parseDouble(priceList.get(3));
+        shortDistancePrice = new BigDecimal(priceList.get(1)).setScale(2, RoundingMode.HALF_UP);
+        mediumDistancePrice = new BigDecimal(priceList.get(2)).setScale(2, RoundingMode.HALF_UP);
+        longDistancePrice = new BigDecimal(priceList.get(3)).setScale(2, RoundingMode.HALF_UP);
 
-        lightLoadPrice = Double.parseDouble(priceList.get(5));
-        mediumLoadPrice = Double.parseDouble(priceList.get(6));
-        hardLoadPrice = Double.parseDouble(priceList.get(7));
+        lightLoadPrice = new BigDecimal(priceList.get(5)).setScale(2, RoundingMode.HALF_UP);
+        mediumLoadPrice = new BigDecimal(priceList.get(6)).setScale(2, RoundingMode.HALF_UP);
+        hardLoadPrice = new BigDecimal(priceList.get(7)).setScale(2, RoundingMode.HALF_UP);
 
     }
 
-    private static double shortDistancePrice;
-    private static double mediumDistancePrice;
-    private static double longDistancePrice;
+    private static BigDecimal shortDistancePrice;
+    private static BigDecimal mediumDistancePrice;
+    private static BigDecimal longDistancePrice;
 
 
-    private static double lightLoadPrice;
-    private static double mediumLoadPrice;
-    private static double hardLoadPrice;
+    private static BigDecimal lightLoadPrice;
+    private static BigDecimal mediumLoadPrice;
+    private static BigDecimal hardLoadPrice;
 
-    public double getDistancePrice(double distance) {
-        if (distance < 1000) {
+    public BigDecimal getDistancePrice(BigDecimal distance) {
+        if (distance.compareTo(BigDecimal.valueOf(1000)) < 0) {
             return shortDistancePrice;
         }
-        if (distance < 5000 && distance > 1000 ) {
+        if (distance.compareTo(BigDecimal.valueOf(1000)) > 0 && distance.compareTo(BigDecimal.valueOf(5000)) < 0) {
             return mediumDistancePrice;
         }
-        if (distance > 5000) {
+        if (distance.compareTo(BigDecimal.valueOf(5000)) > 0) {
             return longDistancePrice;
         }
 
         return mediumDistancePrice;
     }
 
-    public double getLoadPrice(double kg) {
-        if (kg < 500) {
+    public BigDecimal getLoadPrice(BigDecimal weight) {
+        if (weight.compareTo(BigDecimal.valueOf(500)) < 0) {
             return lightLoadPrice;
         }
-        if (kg < 1000 && kg > 500 ) {
+        if (weight.compareTo(BigDecimal.valueOf(500)) > 0 && weight.compareTo(BigDecimal.valueOf(1000)) < 0) {
             return mediumLoadPrice;
         }
-        if (kg > 1000) {
+        if (weight.compareTo(BigDecimal.valueOf(1000)) > 0) {
             return hardLoadPrice;
         }
 
         return mediumLoadPrice;
     }
 }
-
-
-
